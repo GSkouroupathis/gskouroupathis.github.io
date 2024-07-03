64938eaa0ff58e4cc5ed23e675c5a89ec5523314
George Skouroupathis <george_skouroupathis@hotmail.com>
2024-07-03 22:05:40 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-[i].charCodeAt(0) ^ 2);}
-saveAs(blob, 
-.iso");
-application/x-cd-image
-.indexOf("Win")!=-1
+[i].charCodeAt(0) ^ 2);}1
+saveAs(blob, 1
+.iso");1
+application/x-cd-image1
+.indexOf("Win")!=-11
 a
