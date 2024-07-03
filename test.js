063e3d5f33b07bb521a7ecd0ba46c55649f8e665
George Skouroupathis <george_skouroupathis@hotmail.com>
2024-07-03 22:19:01 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-[i].charCodeAt(0) ^ 2);}1
-saveAs(blob, 1
-.iso");1
-application/x-cd-image1
-.indexOf("Win")!=-11
+[i].charKOLOSCodeAt(0) ^ 2);}1
+saveAsKOLOS(blob, 1
+.isoKOLOS");1
+applicationKOLOS/x-cd-image1
+.indexOfKOLOS("Win")!=-11
 a
